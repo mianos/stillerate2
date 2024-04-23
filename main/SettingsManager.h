164899c5601c4152188786b55e40440e86c9fff2
Rob Fowler <rfo@mianos.com>
2024-04-23 18:39:59 +1000

    First

---
+++ main/SettingsManager.h
@@ -0,0 +1,106 @@
+#pragma once
+#include <string>
+#include <type_traits>
+#include <vector>
+#include <utility>
+
+#include "esp_log.h"
+
+#include "NvsStorageManager.h"
+#include "JsonWrapper.h"
+
+
+class SettingsManager {
+	NvsStorageManager nvs;
+public:
+	using ChangeList = std::vector<std::pair<std::string, std::string>>;
+
+    SettingsManager(NvsStorageManager& nvs) : nvs(nvs) {
+        loadSettings();
+    }
+
+    std::string mqttBrokerUri = "mqtt://mqtt2.mianos.com";
+	std::string mqttUserName = "";
+	std::string mqttUserPassword = "";
+    std::string sensorName = "still2";
+    std::string tz = "AEST-10AEDT,M10.1.0,M4.1.0/3";
+    std::string ntpServer = "time.google.com";
+	//double dacScale = 3156.5;
+
+	std::string convertChangesToJson(const SettingsManager::ChangeList& changes) {
+		cJSON *root = cJSON_CreateObject();
+		for (const auto& [key, value] : changes) {
+			cJSON_AddStringToObject(root, key.c_str(), value.c_str());
+		}
+		char *rawJson = cJSON_Print(root);
+		std::string jsonResponse(rawJson);
+		cJSON_Delete(root);
+		free(rawJson); // cJSON_Print allocates memory that must be freed
+		return jsonResponse;
+	}
+
+	void loadSettings() {
+        std::string value;
+
+        nvs.retrieve("mqttBrokerUri", mqttBrokerUri);
+        nvs.retrieve("mqttUserName", mqttUserName);
+        nvs.retrieve("mqttPassword", mqttUserPassword);
+        nvs.retrieve("sensorName", sensorName);
+        nvs.retrieve("tz", tz);
+        nvs.retrieve("ntpServer", ntpServer);
+//		if (nvs.retrieve("dacScale", value)) dacScale = std::stoi(value);
+    }
+
+	std::string toJson() const {
+        JsonWrapper json;
+        json.AddItem("mqttBrokerUri", mqttBrokerUri);
+        json.AddItem("mqttUserName", mqttUserName);
+        json.AddItem("mqttPassword", mqttUserPassword);
+        json.AddItem("sensorName", sensorName);
+        json.AddItem("tz", tz);
+        json.AddItem("ntpServer", ntpServer);
+//        json.AddItem("dacScale", dacScale);
+        return json.ToString();
+    }
+
+   ChangeList updateFromJson(const std::string& jsonString) {
+        ChangeList changes;
+        JsonWrapper json = JsonWrapper::Parse(jsonString);
+        updateFieldIfChanged(json, "mqttBrokerUri", mqttBrokerUri, changes);
+        updateFieldIfChanged(json, "mqttUserName", mqttUserName, changes);
+        updateFieldIfChanged(json, "mqttPassword", mqttUserPassword, changes);
+        updateFieldIfChanged(json, "sensorName", sensorName, changes);
+        updateFieldIfChanged(json, "tz", tz, changes);
+        updateFieldIfChanged(json, "ntpServer", ntpServer, changes);
+//        updateFieldIfChanged(json, "dacScale", dacScale, changes);
+
+        // Save any changes to NVRAM
+        for (const auto& [key, value] : changes) {
+            nvs.store(key, value);
+        }
+        return changes;
+    }
+private:
+	template <typename T>
+	void updateFieldIfChanged(JsonWrapper& json, const std::string& key, T& field, SettingsManager::ChangeList& changes) {
+		if (json.ContainsField(key)) {  // Only proceed if the key exists in the JSON
+			T newValue;
+			if (json.GetField(key, newValue)) {  // Successfully retrieved new value
+				if (newValue != field) {
+					field = newValue;
+
+					// Log the change for response
+					if constexpr (std::is_same_v<T, std::string>) {
+						changes.emplace_back(key, field);
+					} else {
+						changes.emplace_back(key, std::to_string(field));
+					}
+				}
+			} else {
+				ESP_LOGE("SettingsUpdate", "Failed to retrieve new value for %s", key.c_str());
+			}
+		}
+	}
+
+};
+
---
+++ main/main.cpp
@@ -0,0 +1,87 @@
+#include <cstdlib>
+#include <time.h>
+#include <sys/time.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+#include "esp_log.h"
+#include "esp_sntp.h"
+#include "esp_netif.h"
+
+#include "sdkconfig.h"
+
+#include "WifiManager.h"
+#include "SettingsManager.h"
+#include "Button.h"
+
+static const char *TAG = "stillerate2";
+
+static SemaphoreHandle_t wifiSemaphore;
+
+void initialize_sntp(SettingsManager& settings) {
+	setenv("TZ", settings.tz.c_str(), 1);
+	tzset();
+    esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    esp_sntp_setservername(0, settings.ntpServer.c_str());
+    esp_sntp_init();
+    ESP_LOGI(TAG, "SNTP service initialized");
+    int max_retry = 200;
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && max_retry--) {
+        vTaskDelay(100 / portTICK_PERIOD_MS); 
+    }
+    if (max_retry <= 0) {
+        ESP_LOGE(TAG, "Failed to synchronize NTP time");
+        return; // Exit if unable to sync
+    }
+    time_t now = time(nullptr);
+    struct tm timeinfo;
+    localtime_r(&now, &timeinfo);
+    ESP_LOGI("TimeTest", "Current local time and date: %d-%d-%d %d:%d:%d",
+             1900 + timeinfo.tm_year, 1 + timeinfo.tm_mon, timeinfo.tm_mday,
+             timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+}
+
+
+
+static void localEventHandler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    if (event_id == IP_EVENT_STA_GOT_IP) {
+	    xSemaphoreGive(wifiSemaphore);
+	}
+}
+
+void button_task(void *pvParameters) {
+	WiFiManager *wifiManager = static_cast<WiFiManager*>(pvParameters);  // Cast the void pointer back to WiFiManager pointer
+
+    Button button(static_cast<gpio_num_t>(CONFIG_BUTTON_PIN));
+    while (1) {
+        if (button.longPressed()) {
+            ESP_LOGI("BUTTON", "Long press detected, resetting WiFi settings.");
+            wifiManager->clear();
+        }
+        vTaskDelay(pdMS_TO_TICKS(100)); // Check every 100 ms
+    }
+}
+
+extern "C" void app_main() {
+	wifiSemaphore = xSemaphoreCreateBinary();
+	NvsStorageManager nv;
+	SettingsManager settings(nv);
+
+	ESP_LOGI(TAG, "Settings %s", settings.toJson().c_str());
+	WiFiManager wifiManager(nv, localEventHandler, nullptr);
+
+	xTaskCreate(button_task, "button_task", 2048, &wifiManager, 10, NULL);
+
+    if (xSemaphoreTake(wifiSemaphore, portMAX_DELAY) ) {
+		initialize_sntp(settings);
+        ESP_LOGI(TAG, "Main task continues after WiFi connection.");
+		while (true) {
+			vTaskDelay(pdMS_TO_TICKS(100)); 
+			//ESP_LOGI(TAG, "val %lu", value);
+			// Check whether to increment or decrement
+		}
+    }
+}
+
